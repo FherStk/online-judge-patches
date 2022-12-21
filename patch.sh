81e9afffac47b809cc0aacd52ad188b4c03ec678
FherStk <porrino.fernando@gmail.com>
2022-12-21 11:55:34 +0100

    fixes

--- patch.sh
+++ patch.sh
@@ -2,18 +2,21 @@
 PATCH="2022-12-20"
 
 cd ..
-echo -n "Backing up... "
+echo "Backing up:"
 cp -Rrf online-judge-custom online-judge-custom.backup
-echo "OK"
+cp -Rrf online-judge-source online-judge-source.backup
+echo "Done!"
 echo
 
 echo "Undoing custom changes:"
 cd online-judge-custom
 git reset origin/master --hard
+echo "Done!"
 echo
 
 echo "Downloading remote changes"
 git pull origin master
+echo "Done!"
 echo
 
 echo "Patching with custom changes:"
@@ -22,3 +25,15 @@ for f in online-judge-patches/$PATCH/*; do
     patch -p1 -f < "$f"
 done
 echo "Done!"
+echo 
+
+echo "Setting up project folders:"
+rm -rf online-judge-custom
+cp -r online-judge-source online-judge-custom
+
+rm -rf online-judge-source
+cp -r online-judge-source.backup online-judge-source
+echo "Done!"
+echo
+
+echo "Finished!"
